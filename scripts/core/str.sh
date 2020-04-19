d11173dafca193c55f68b3846342fb949442e711
Borja Paz Rodríguez <borjapazr@gmail.com>
2020-04-19 23:18:27 +0200

    😢 Add str::function()

--- scripts/core/str.sh
+++ scripts/core/str.sh
@@ -6,3 +6,14 @@ str::split() {
 
   echo "$text" | tr "$splitter" "\n"
 }
+
+str::urlencode() {
+   local data
+   data="$(curl -s -o /dev/null -w %{url_effective} --get --data-urlencode "$1" "")"
+   if [[ $? != 3 ]]; then
+      echo "Unexpected error" 1>&2
+      return 2
+   fi
+   echo "${data##/?}"
+   return 0
+}
