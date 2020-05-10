2b6ad3d4d7546885d048e3699f0e294f7a49ff5e
Borja Paz Rodríguez <borjapazr@gmail.com>
2020-05-11 00:39:51 +0200

    ✨ echos function

--- shell/_functions/utils.sh
+++ shell/_functions/utils.sh
@@ -9,3 +9,11 @@ pbcopy() {
 pbpaste() {
    dot system clip paste
 }
+
+echos() {
+   limit="${1:-22}"
+   for i in {1..$limit}
+      do
+         echo "--------------------------------------------------------------------------------"
+      done
+}
