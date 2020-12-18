7eb1fb989570958a38441a99ecb495e6a1d52528
Borja Paz Rodríguez <borjapazr@gmail.com>
2020-12-18 17:27:01 +0100

    🔧 Add emojis to root user and ssh session

--- shell/bash/themes/mrmars.sh
+++ shell/bash/themes/mrmars.sh
@@ -7,6 +7,15 @@ GREEN_COLOR="32"
 RED_COLOR="31"
 
 mrmars_theme() {
+  local root_emoji
+  if [[ $USER == 'root' ]]; then
+    root_emoji="✨"
+  fi
+  local cloud_emoji
+  if [[ -n $SSH_CONNECTION ]]; then
+    cloud_emoji="☁️ "
+  fi
+
   LAST_CODE="$?"
   CURRENT_DIR=$(dot filesystem short_pwd)
   STATUS_COLOR=$GREEN_COLOR
@@ -15,6 +24,6 @@ mrmars_theme() {
     STATUS_COLOR=$RED_COLOR
   fi
 
-  export PS1="\[\e[1m\]\[\e[${STATUS_COLOR}m\][\[\e[${YELLOW_COLOR}m\]s\[\e[${GREEN_COLOR}m\]r\[\e[${RED_COLOR}m\]a\[\e[${BLUE_COLOR}m\]m\[\e[${STATUS_COLOR}m\]]\[\e[m\]\[\e[m\] \[\e[1m\]\[\e[${CYAN_COLOR}m\]${CURRENT_DIR}\[\e[m\]\[\e[m\] "
+  export PS1="\[\e[1m\]\[\e[${STATUS_COLOR}m\][\[\e[${YELLOW_COLOR}m\]s\[\e[${GREEN_COLOR}m\]r\[\e[${RED_COLOR}m\]a\[\e[${BLUE_COLOR}m\]m\[\e[${STATUS_COLOR}m\]]\[\e[m\]\[\e[m\]$cloud_emoji$root_emoji \[\e[1m\]\[\e[${CYAN_COLOR}m\]${CURRENT_DIR}\[\e[m\]\[\e[m\] "
 
 }
