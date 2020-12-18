edd896d4057da49b257ad2c194e37f24558ea338
Borja Paz Rodríguez <borjapazr@gmail.com>
2020-12-18 23:31:52 +0100

    🔧 Update remote emoji

--- shell/bash/themes/mrmars.sh
+++ shell/bash/themes/mrmars.sh
@@ -6,16 +6,16 @@ YELLOW_COLOR="33"
 GREEN_COLOR="32"
 RED_COLOR="31"
 
-mrmars_theme() {
-  local root_emoji
-  if [[ $USER == 'root' ]]; then
-    root_emoji="✨"
-  fi
-  local cloud_emoji
-  if [[ -n $SSH_CONNECTION ]]; then
-    cloud_emoji="☁️ "
-  fi
+ROOT_EMOJI=""
+if [[ $USER == 'root' ]]; then
+  ROOT_EMOJI="✨"
+fi
+REMOTE_EMOJI=""
+if [[ $IS_SSH_CONNECTION == 1 ]]; then
+  REMOTE_EMOJI="☁️ "
+fi
 
+mrmars_theme() {
   LAST_CODE="$?"
   CURRENT_DIR=$(dot filesystem short_pwd)
   STATUS_COLOR=$GREEN_COLOR
@@ -24,6 +24,6 @@ mrmars_theme() {
     STATUS_COLOR=$RED_COLOR
   fi
 
-  export PS1="\[\e[1m\]\[\e[${STATUS_COLOR}m\][\[\e[${YELLOW_COLOR}m\]s\[\e[${GREEN_COLOR}m\]r\[\e[${RED_COLOR}m\]a\[\e[${BLUE_COLOR}m\]m\[\e[${STATUS_COLOR}m\]]\[\e[m\]\[\e[m\]$cloud_emoji$root_emoji \[\e[1m\]\[\e[${CYAN_COLOR}m\]${CURRENT_DIR}\[\e[m\]\[\e[m\] "
+  export PS1="\[\e[1m\]\[\e[${STATUS_COLOR}m\][\[\e[${YELLOW_COLOR}m\]s\[\e[${GREEN_COLOR}m\]r\[\e[${RED_COLOR}m\]a\[\e[${BLUE_COLOR}m\]m\[\e[${STATUS_COLOR}m\]]\[\e[m\]\[\e[m\]${REMOTE_EMOJI}${ROOT_EMOJI} \[\e[1m\]\[\e[${CYAN_COLOR}m\]${CURRENT_DIR}\[\e[m\]\[\e[m\] "
 
 }
