fb0f2f0ce1a75bf24ed2aeefbdfba04f59a20780
Borja Paz Rodríguez <borjapazr@gmail.com>
2020-12-18 22:00:46 +0100

    🐛 Fix root indicator

--- shell/bash/themes/mrmars.sh
+++ shell/bash/themes/mrmars.sh
@@ -8,10 +8,10 @@ RED_COLOR="31"
 
 mrmars_theme() {
   local root_emoji
-  local cloud_emoji
   if [[ $USER == 'root' ]]; then
     root_emoji="✨"
   fi
+  local cloud_emoji
   if [[ -n $SSH_CONNECTION ]]; then
     cloud_emoji="☁️ "
   fi
@@ -24,6 +24,6 @@ mrmars_theme() {
     STATUS_COLOR=$RED_COLOR
   fi
 
-  export PS1="\[\e[1m\]\[\e[${STATUS_COLOR}m\][\[\e[${YELLOW_COLOR}m\]s\[\e[${GREEN_COLOR}m\]r\[\e[${RED_COLOR}m\]a\[\e[${BLUE_COLOR}m\]m\[\e[${STATUS_COLOR}m\]]\[\e[m\]\[\e[m\]${cloud_emoji}${root_emoji} \[\e[1m\]\[\e[${CYAN_COLOR}m\]${CURRENT_DIR}\[\e[m\]\[\e[m\] "
+  export PS1="\[\e[1m\]\[\e[${STATUS_COLOR}m\][\[\e[${YELLOW_COLOR}m\]s\[\e[${GREEN_COLOR}m\]r\[\e[${RED_COLOR}m\]a\[\e[${BLUE_COLOR}m\]m\[\e[${STATUS_COLOR}m\]]\[\e[m\]\[\e[m\]$cloud_emoji$root_emoji \[\e[1m\]\[\e[${CYAN_COLOR}m\]${CURRENT_DIR}\[\e[m\]\[\e[m\] "
 
 }
