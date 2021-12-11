d2881d7e3de5a5fbc312b06b61bc0163682b7499
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-11 01:09:54 +0100

    fix(git): add missing git auxiliary function

--- scripts/core/git.sh
+++ scripts/core/git.sh
@@ -1,3 +1,11 @@
+git::is_in_repo() {
+  git rev-parse HEAD >/dev/null 2>&1
+}
+
+git::current_branch() {
+  git branch
+}
+
 git::clone_if_not_exists() {
   if [ ! -d "$2" ]; then
     git clone $1 $2
