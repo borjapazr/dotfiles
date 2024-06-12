be7f60e9e341beffb577f73490251e5d18e4c539
Borja Paz Rodríguez <borjapazr@gmail.com>
2024-06-12 18:25:13 +0200

    feat(docker): update docker recipe

--- scripts/self/recipes/docker.sh
+++ scripts/self/recipes/docker.sh
@@ -2,14 +2,16 @@
 docker::install() {
   if platform::is_linux; then
     if platform::command_exists apt-mark; then
-      sudo apt-get remove -y docker docker-engine docker.io containerd runc 2>&1 | log::file "🆑 Removing old docker packages"
+      sudo apt-get remove -y docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc 2>&1 | log::file "🆑 Removing old docker packages"
       sudo apt-get install -y ca-certificates curl gnupg lsb-release 2>&1 | log::file "📥 Installing docker dependencies"
-      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 2>&1 | log::file "➕ Adding docker GPG keys"
+      sudo install -m 0755 -d /etc/apt/keyrings 2>&1 | log::file "✅ Created /etc/apt/keyrings directory with permissions 0755"
+      sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc 2>&1 | log::file "📥 Downloaded Docker GPG key to /etc/apt/keyrings/docker.asc"
+      sudo chmod a+r /etc/apt/keyrings/docker.asc 2>&1 | log::file "🔒 Set read permissions for all users on Docker GPG key file"
       echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-          $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null | log::file "➕ Adding docker repository"
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null | log::file "➕ Adding docker repository"
       sudo apt-get update -y 2>&1 | log::file "🔃 Updating system"
-      sudo apt-get install -y docker-ce docker-ce-cli containerd.io 2>&1 | log::file "📥 Installing docker"
+      sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 2>&1 | log::file "📥 Installing docker"
     fi
   fi
 }
