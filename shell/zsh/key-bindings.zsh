d1e23f0312f07b1f307811e7293d8bb0c0df45a8
Borja Paz Rodríguez <borjapazr@gmail.com>
2025-08-13 19:52:51 +0200

    feat(zsh): add history substring search key bindings

--- shell/zsh/key-bindings.zsh
+++ shell/zsh/key-bindings.zsh
@@ -14,3 +14,13 @@ fzf_show_dot_commands() {
 }
 zle -N fzf_show_dot_commands
 bindkey '^E' fzf_show_dot_commands
+
+# zsh-users/zsh-history-substring-search
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
