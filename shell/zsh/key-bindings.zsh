9bff85813add673c305c9207ddcea1cba45e56fd
Borja Paz Rodríguez <borjapazr@gmail.com>
2025-08-13 19:59:03 +0200

    refactor(key-bindings): simplify history substring search key bindings

--- shell/zsh/key-bindings.zsh
+++ shell/zsh/key-bindings.zsh
@@ -16,11 +16,8 @@ zle -N fzf_show_dot_commands
 bindkey '^E' fzf_show_dot_commands
 
 # zsh-users/zsh-history-substring-search
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-bindkey -M emacs '^P' history-substring-search-up
-bindkey -M emacs '^N' history-substring-search-down
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+for key ('^[[A' '^P' ${terminfo[kcuu1]}) bindkey ${key} history-substring-search-up
+for key ('^[[B' '^N' ${terminfo[kcud1]}) bindkey ${key} history-substring-search-down
+for key ('k') bindkey -M vicmd ${key} history-substring-search-up
+for key ('j') bindkey -M vicmd ${key} history-substring-search-down
+unset key
